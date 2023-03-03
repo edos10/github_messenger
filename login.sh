48e7cd9b95a177854407432ae90c8ae1833ab74a
edos10 <riabov.edik201533@gmail.com>
2023-03-03 23:28:39 +0300

    fix login script

--- login.sh
+++ login.sh
@@ -1,1 +1,25 @@
-#!/bin/bash                                                                                                                                                                                                                                     echo "Введите имя пользователя:"                                                                                        read username                                                                                                           echo "Введите пароль:"                                                                                                  read -s password                                                                                                        response=$(curl -s -u "$username:$password" -X POST https://api.github.com/authorizations \                             -d "{\"scopes\":[\"repo\"],\"note\":\"github-messenger\"}")                                                             token=$(echo $response | grep -oP '"token": "\K[^"]+')                                                                  echo $token > ~/.github_token                                                                                           echo "Вы успешно вошли на GitHub! Токен доступа сохранен в ~/.github_token" 
+#!/bin/bash
+
+# Запрос имени пользователя и пароля
+
+echo "Введите имя пользователя:"
+
+read username
+
+echo "Введите пароль:"
+
+read -s password
+
+# Отправка запроса на авторизацию и получение токена доступа
+
+response=$(curl -s -u "$username:$password" -X POST https://api.github.com/authorizations \-d "{\"scopes\":[\"repo\"],\"note\":\"github-messenger\"}")
+
+token=$(echo $response | grep -oP '"token": "\K[^"]+')
+
+# Сохранение токена в файл
+
+echo $token > ~/.github_token
+
+# Вывод сообщения об успешной авторизации
+
+echo "Вы успешно вошли на GitHub. Токен доступа сохранен в ~/.github_token"
